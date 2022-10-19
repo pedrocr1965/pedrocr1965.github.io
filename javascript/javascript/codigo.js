a79c7e815d9b2d4e5b2ae7c258467ba6e6e730aa
pedrocr1965 <pedro.r@queretaro.tecnm.mx>
2022-10-19 18:47:49 -0500

    suma

--- javascript/javascript/codigo.js
+++ javascript/javascript/codigo.js
@@ -3,9 +3,9 @@ nombre: "Juan",
 edad: 30
 };
 function calcula(){
-    var operando1 = Number(document.forms[0].operando1.value)
-    var operando2 = Number(document.forms[0].operando2.value)
-    var operador = Number(document.forms[0].operador.value)
+    var operando1 = document.forms[0].operando1.value
+    var operando2 = document.forms[0].operando2.value
+    var operador = document.forms[0].operador.value
     var result = eval(operando1 + operador + operando2)
     document.forms[0].resultado.value = result
 }
